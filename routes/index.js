b53045d9d0aa7b3f02e70b5b2832fb9be9807fb1
AlexTaures <alextauresdiaz@gmail.com>
2023-08-15 14:36:05 -0600

    deleted useless lines

--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,11 @@
-// const fs = require('fs/promises');
-// const path = require('path')
-
-// const routes = [];
-
-// const loadRoutes = async (path) => {
-//     try {
-//       const files = await fs.readdir(path);
-//       // console.log({
-//       //   path: path,
-//       //   files: files
-//       // })
-//       const regex = /\//; 
-//       for (const file of files) {
-//         if (file !== 'index.js' && !regex.test(file)) {
-//           const name = file.replace(/\.js$/, '');
-//           routes.push({
-//               path: `/api/${name}`,
-//               fileDir: `../routes/${name}`
-//           })
-//         }
-//       }
-//     } catch (error) {
-//       console.error('Error reading folder:', error);
-//       throw error;
-//     }
-//     //console.log(routes)
-//     return routes;
-// };
-
-// module.exports = { loadRoutes };
-
-
-
-////////////
-//const fs = require('fs/promises');
-const path = require('path')
 const fs = require('fs');
 const yaml = require('js-yaml');
 
-// try {
-//   const yamlData = fs.readFileSync(__dirname+'/index.yml', 'utf8');
-//   const dataArray = yaml.load(yamlData);
-//   console.log(dataArray); // This will output the array from the YAML file
-// } catch (error) {
-//   console.error(error);
-// }
-
 const routes = [];
+
 const loadRoutes = async (path) => {
     try {
-        const yamlData = fs.readFileSync(path+'/index.yml');
+        const yamlData = fs.readFileSync(path+'/parameters.yml');
         const files = yaml.load(yamlData);
       for (const file of files) {
           routes.push({
@@ -62,7 +18,6 @@ const loadRoutes = async (path) => {
       console.error('Error reading folder:', error);
       throw error;
     }
-    //console.log(routes)
     return routes;
 };
 
