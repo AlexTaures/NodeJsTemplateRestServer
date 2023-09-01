417ff9918e14d487ec41ae990aefc0380af79316
AlexTaures <alextauresdiaz@gmail.com>
2023-09-01 09:21:43 -0600

    adding new function to generate-key script

--- helpers/generate-s-key.js
+++ helpers/generate-s-key.js
@@ -4,6 +4,9 @@ const crypto = require('crypto');
 const { colors } = require('colors');
 const mongoose = require('mongoose');
 const User = require('../models/user');
+const readline = require('readline');
+
+const keyName = 'SECRETORPRIVATEKEY';
 
 //Connect to database
 dotenv.config();
@@ -19,7 +22,6 @@ const verificateUsers = async () => {
   } catch (error) {
     throw error;
   }
-
 }
 
 // Generate a random secret key
@@ -28,6 +30,21 @@ const generateSecretKey = () => {
   return key.toString('hex');
 };
 
+//Set content to write en .env file
+const setNewEnvContent = (newKey) => {
+  let newFileContent = [];
+  const fileContent = fs.readFileSync('.env', 'utf-8').split('\n');
+  fileContent.forEach((line, index) => {
+    if(line.includes(keyName)){
+      //let key = line.replace(/\s/g, '').trim().replace(`${keyName}=`,''); //To obtain current key
+      line = `${keyName}=${newKey}`;
+    }
+    newFileContent[index] = line;
+  });
+  return newFileContent.join('\n');
+}
+
+//Main function 
 const main =  async () => {
   mongoose.connect(mongodbUri, {
     useNewUrlParser: true,
@@ -36,30 +53,37 @@ const main =  async () => {
   if( await verificateUsers() || forceFlag ){
     try {
       const secretKey = generateSecretKey();
-  
-      const envFile = dotenv.parse(fs.readFileSync('.env'));
-      envFile.SECRETORPRIVATEKEY = secretKey;
-      const updatedEnvContent = Object.entries(envFile)
-        .map(([key, value]) => `${key}=${value}`)
-        .join('\n');
-      fs.writeFileSync('.env', updatedEnvContent);
+      
+      fs.writeFileSync('.env', setNewEnvContent(secretKey));
       console.log('Key generated successfully!!!!!!'.green);
     } catch (error) {
       console.log(error.red);
     }
   }else{
     console.log('There are user data in database, is not recomendable generate a new key.'.yellow);
-    console.log('To force key generation exec command line:'.yellow+' npm run generate-key -- --force '.blue);
+    console.log('To force key generation exec command line:'.yellow+' npm run generate-key -- --force ');
   }
   mongoose.disconnect();
 }
 
-main();
 
+if(forceFlag){
+  const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  console.log('Generating a new key could affect reading already registered passwords.\nAre you sure you want to generate a new secret key? [Yes/~else]): '.blue);
+  //reader.question();
+  reader.on('line', (res) => {
+    if (res.toLowerCase() !== 'yes') {
+      console.log('Key generation aborted.'.yellow);
+    }else{
+      main();
+    }
 
-
-
-
-
-
+    reader.close();
+  });
+}else{
+  main();
+}
 
